fc095d11bedc52e4b4afa7873de6e1c94262cdea
brandaochico <brandao.chico24@gmail.com>
2025-06-24 08:09:29 -0300

    importando mapa e objetos do Tiled

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame.sprite
+from pytmx.util_pygame import load_pygame
 
 from settings import *
 from os.path import join
@@ -20,12 +21,23 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
+        # setup
+        self.setup()
+
         # sprites
-        self.player = Player((WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60, 100), randint(50, 100)
-            ColissionSprite((x,y), (w, h), (self.all_sprites, self.collision_sprites))
+        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
+
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
 
 
     def run(self):
